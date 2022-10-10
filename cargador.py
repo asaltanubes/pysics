ce6092b633b982751a35900584fe7e40709647bb
asaltanubes <gustavodragonico@gmail.com>
2022-10-10 22:42:55 +0200

    Cargar ahora carga los datos por columnas en vez de por filas

--- cargador.py
+++ cargador.py
@@ -1,6 +1,11 @@
-def cargar(archivo: str, separador: str = '\t', linea: str = '\n', decimal: str = ',',cabeceras: int = 0, data_type: type = float) -> list[list[str]]:
+def cargar(archivo: str, separador: str = '\t', linea: str = '\n', decimal: str = ',', cabeceras: int = 0, transposed=True) -> list[list[str]]:
     with open(archivo, 'rt') as file:
-        data = file.read().strip()
-        filas = [i.strip() for i in data.split(linea)]
-        datos = [[data_type(i.strip().replace(decimal, '.')) for i in j.split(separador)] for j in filas[cabeceras:]]
+        data = file.read()
+        filas = [i for i in data.split(linea) if i.strip() != '']
+        datos = [[float(i.strip().replace(decimal, '.')) if i.strip() != '' else None for i in j.split(separador) ] for j in filas[cabeceras:]]
+    
+    if transposed:
+        max_len = max((len(i) for i in datos))        
+        datos = [[i[index] for i in datos if index < len(i) and i[index] != None] for index in range(max_len)]
+    
     return datos
