2f526e7a6cbd105fb315dfd5a6c35d2bb6ea67f5
asaltanubes <gustavodragonico@gmail.com>
2022-10-11 11:27:22 +0200

    pp

--- cargador.py
+++ cargador.py
@@ -1,10 +1,10 @@
-def cargar(archivo: str, separador: str = '\t', linea: str = '\n', decimal: str = ',', cabeceras: int = 0, transposed=True) -> list[list[str]]:
+def cargar(archivo: str, separador: str = '\t', linea: str = '\n', decimal: str = ',', cabeceras: int = 0, por_columnas=True) -> list[list[float]]:
     with open(archivo, 'rt') as file:
         data = file.read()
         filas = [i for i in data.split(linea) if i.strip() != '']
         datos = [[float(i.strip().replace(decimal, '.')) if i.strip() != '' else None for i in j.split(separador) ] for j in filas[cabeceras:]]
     
-    if transposed:
+    if por_columnas:
         max_len = max((len(i) for i in datos))        
         datos = [[i[index] for i in datos if index < len(i) and i[index] != None] for index in range(max_len)]
     
