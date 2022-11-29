441c709559bf946a6906c0665eb0f3534b9eb14a
asaltanubes <gustavodragonico@gmail.com>
2022-11-29 16:59:51 +0100

    Tabla funciona con versiones de tabulate 0.8.9<

--- tabla.py
+++ tabla.py
@@ -21,7 +21,7 @@ def create_table_list(lista: list, cabecera=None, transponer=False) -> list[list
         lista = [[h] + l for h, l in zip(cabecera, lista)]
     if not transponer:
         lista = transpose(lista)
-    return lista
+    return [[str(l) for l in j] for j in lista]
 
 def terminal(datos: list, cabecera: Opcional[list] = None, transponer: bool = False) -> str:
     """Devuelve una cadena de texto que representa una tabla que se puede mostrar por la terminal"""
@@ -29,7 +29,7 @@ def terminal(datos: list, cabecera: Opcional[list] = None, transponer: bool = Fa
 
 def latex(datos: list, cabecera: Opcional[list] = None, caption: str = 'Caption', label: str = 'tab:my_label', transponer: bool = False) -> str:
     """Devuelve una cadena de texto que representa una tabla de latex"""
-    tabular = '\n'.join(tabulate(create_table_list(datos, cabecera, transponer), tablefmt='latex').replace(r'\\', '\\\\ \n \\hline').split('\n')[1:-2])
+    tabular = '\n'.join(tabulate(create_table_list(datos, cabecera, transponer), tablefmt='latex').replace(r'\\', '\\\\ \n \\hline').replace(r"\$", r"$").replace(r'\textbackslash{}', "\\").split('\n')[1:-2])
     ancho = len(tabular.split('\\hline')[1].split('&'))
     tabular = f'\\begin{"{"}tabular{"}"}{"{"}|{"|".join(["c"]*ancho)}|{"}"}\n' + tabular + '\n' + r'\end{tabular}'
     table = '\\begin{table}[ht]\n \\centering \n\n' + tabular + '\n\n' + '\\caption{' + caption + '}\n\\label{' + label + '}\n\\end{table}'
