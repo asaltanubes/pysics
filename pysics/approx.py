Update package name to pysics, add email of author in pyproject.toml. Done some spell check and fix print units with numbers.

pysics/approx.py
@@ -1,10 +1,10 @@
 import numpy as np
 from math import trunc, isnan, isinf, nan
-from . import calculos
+from . import round
 
-def aprox(value: list[float], error: list[float]) -> tuple[list[float], list[float]]:
+def approx(value: list[float], error: list[float]) -> tuple[list[float], list[float]]:
     """
-    Aproximate a value and its error to the first significant figure of the error (3.894 ± 0.26 -> 4.0 ± 0.3) or to the first two if the first is 1 (3.834 ± 0.169 -> 3.83 ± 0.17)
+    approximate a value and its error to the first significant figure of the error (3.894 ± 0.26 -> 4.0 ± 0.3) or to the first two if the first is 1 (3.834 ± 0.169 -> 3.83 ± 0.17)
     If a list of values is passed, it is applied to each pair separately
 
     Args:
@@ -16,15 +16,15 @@ def aprox(value: list[float], error: list[float]) -> tuple[list[float], list[flo
     """
     # If error is not an iterable object then the version that does not iterate is applied. If it is iterable then the version that iterates is applied
     if not hasattr(value, '__iter__'):
-        return apr(value, error)
-    p = apr_list(value, error)
+        return appr(value, error)
+    p = appr_list(value, error)
     return p
 
 def truncate(number: float, digits: int) -> float:
     """
     Truncate a value with n digits where n is the number of digits after the "."
     examples(truncate(10.93, 1) -> 10.9; truncate(10.935, 2) -> 10.93; truncate(10.720, 0) -> 10; truncate(11.111, -1) -> 10)
-    
+
     Args:
         number (float): value to truncate
         digits (int): number of digits to take after the .
@@ -48,9 +48,9 @@ def significative_figure(num: float) -> int:
     """
     return np.floor(np.log10(abs(num)))
 
-def apr(value: float, error: float) -> tuple[float, float]:
+def appr(value: float, error: float) -> tuple[float, float]:
     """
-    Aproximate a value and its error to the first significant figure of the error (3.894 ± 0.26 -> 4.0 ± 0.3) or to the first two if the first is 1 (3.834 ± 0.169 -> 3.83 ± 0.17)
+    approximate a value and its error to the first significant figure of the error (3.894 ± 0.26 -> 4.0 ± 0.3) or to the first two if the first is 1 (3.834 ± 0.169 -> 3.83 ± 0.17)
 
     Args:
         value (float): value to approximate
@@ -59,30 +59,30 @@ def apr(value: float, error: float) -> tuple[float, float]:
     Returns:
         tuple[float, float]: (value, error) returns the value and the error approximated
     """
-    
+
     if error == 0 or isnan(error):
         return (value, error)
     if isnan(value):
-        return (nan, apr(1, error)[1])
+        return (nan, appr(1, error)[1])
     if isinf(error):
         return (0, error)
     if isinf(value):
-        return (value, apr(1, error)[1])
-    
+        return (value, appr(1, error)[1])
+
     error_figures = -significative_figure(error)
     a = truncate(error, error_figures)
-    
+
     # If the first significant figure is 1
     if np.log10(a) == np.floor(np.log10(a)):
         # If when approximating to the next one the result is 1 then the next one is also taken.
         # I check if it is less than 2 for possible floating point errors. I hate floating point arithmetic.
-        if calculos.round(error, error_figures) < 2*10**(-error_figures):
+        if round.round(error, error_figures) < 2*10**(-error_figures):
             error_figures += 1
-    return (calculos.round(value, error_figures), calculos.round(error, error_figures))
+    return (round.round(value, error_figures), round.round(error, error_figures))
 
-def apr_list(value: list[float], error: list[float]) -> tuple[list[float], list[float]]:
+def appr_list(value: list[float], error: list[float]) -> tuple[list[float], list[float]]:
     """
-    Apply apr to a list of values and errors
+    Apply appr to a list of values and errors
     """
     if not isinstance(value, np.ndarray):
         value = np.array(value)
@@ -92,7 +92,7 @@ def apr_list(value: list[float], error: list[float]) -> tuple[list[float], list[
     vallist = []
     errlist = []
     for i in zip(value, error):
-        v, e = apr(*i)
+        v, e = appr(*i)
         vallist.append(v)
         errlist.append(e)
     vallist = np.array(vallist)
@@ -126,4 +126,4 @@ def apr_list(value: list[float], error: list[float]) -> tuple[list[float], list[
 if __name__ == '__main__':
     a = [8.365241, 935.27, 89523.68586583]
     e = [.35, 1.34, 1.389656]
-    print(aprox(a, e))
+    print(approx(a, e))

pysics/fit.py
@@ -4,7 +4,7 @@ from scipy.optimize import curve_fit
 from inspect import signature
 
 
-def curve(function, x: list[float], y: list[float], sigma = None, initial_guess: list[float] | float | None = None, aproximate: bool = False):
+def curve(function, x: list[float], y: list[float], sigma = None, initial_guess: list[float] | float | None = None, approximate: bool = False):
     """
     Makes a fit to an arbitrary curve given by the function passed as a parameter.
     If no parameter is passed for sigma, the fit has no weights, if an iterable is passed, the values are taken as errors
@@ -29,7 +29,7 @@ def curve(function, x: list[float], y: list[float], sigma = None, initial_guess:
 
     popt, error = curve_fit(function, x, y, p0=initial_guess, sigma = sigma)
     # Returns a tuple with the Measures obtained
-    return tuple((Measure(v, e, aproximate=aproximate) for v, e in zip(popt, np.sqrt(np.diag(error)))))
+    return tuple((Measure(v, e, approximate=approximate) for v, e in zip(popt, np.sqrt(np.diag(error)))))
 
 def r_line(x: list[float], y: list[float]):
     x = np.array(x)
@@ -40,7 +40,7 @@ def r_line(x: list[float], y: list[float]):
     sigma_y = sum(des_y**2)
     return sum(des_x * des_y)/np.sqrt(sigma_x*sigma_y)
 
-def r_curve(function, x, y, sigma = None, initial_guess=None, aproximate = False) -> float:
+def r_curve(function, x, y, sigma = None, initial_guess=None, approximate = False) -> float:
     if isinstance(y, Measure):
         if hasattr(sigma, '__iter__'):
             sigma = y.error
@@ -60,7 +60,7 @@ def r_curve(function, x, y, sigma = None, initial_guess=None, aproximate = False
     ss_tot = np.sum((y-np.mean(y))**2)
     return np.sqrt(1 - (ss_res / ss_tot))
 
-def least_squares(x: list[float], y: list[float], aproximate: bool = False) -> Line:
+def least_squares(x: list[float], y: list[float], approximate: bool = False) -> Line:
     """
     Calculates the Line of adjustment by least squares for two Measures.
 
@@ -73,13 +73,13 @@ def least_squares(x: list[float], y: list[float], aproximate: bool = False) -> L
     dslope, dn_0 = sigma_calc_line(x, y)
     x_units  = x.units if isinstance(x, Measure) else Units()
     y_units = y.units if isinstance(y, Measure) else Units()
-    slope: Measure = Measure(slope, dslope, aproximate=aproximate, units=y_units/x_units)
-    n_0: Measure = Measure(n_0, dn_0, aproximate=aproximate, units=y_units)
+    slope: Measure = Measure(slope, dslope, approximate=approximate, units=y_units/x_units)
+    n_0: Measure = Measure(n_0, dn_0, approximate=approximate, units=y_units)
     if isinstance(x, Measure):
         x = x.value
     return Line(slope, n_0, x)
 
-def wleast_squares(x: Measure, y: Measure, yerr: list[float] = None, aproximate: bool = False) -> Line:
+def wleast_squares(x: Measure, y: Measure, yerr: list[float] = None, approximate: bool = False) -> Line:
     """
     Calculates the Line of adjustment by weighted least squares for two Measures.
     The error of the y axis is extracted from the Measure \"y\" and the error of the x axis is
@@ -97,8 +97,8 @@ def wleast_squares(x: Measure, y: Measure, yerr: list[float] = None, aproximate:
     dslope, dn_0 = wsigma_calc_line(x=x, y=y, yerr=yerr)
     x_units  = x.units if isinstance(x, Measure) else Units()
     y_units = y.units if isinstance(y, Measure) else Units()
-    slope: Measure = Measure(slope, dslope, aproximate=aproximate, units=x_units)
-    n_0: Measure = Measure(n_0, dn_0, aproximate=aproximate, units=y_units)
+    slope: Measure = Measure(slope, dslope, approximate=approximate, units=x_units)
+    n_0: Measure = Measure(n_0, dn_0, approximate=approximate, units=y_units)
     if isinstance(x, Measure):
         x = x.Measure
     return Line(slope, n_0, x)

pysics/func.py
@@ -32,7 +32,7 @@ def sin(x: Measure) -> Measure:
     for i in nullvalues:
         error[i] = np.abs(np.sin(x._value[i]+x._error[i])-np.sin(x._value[i]))
 
-    return Measure(value, error, aproximate = False)
+    return Measure(value, error, approximate = False)
 
 def cos(x: Measure) -> Measure:
     """
@@ -54,7 +54,7 @@ def cos(x: Measure) -> Measure:
     for i in nullvalues:
         error[i] = np.abs(np.cos(x._value[i]+x._error[i])-np.cos(x._value[i]))
 
-    return Measure(value, error, aproximate = False)
+    return Measure(value, error, approximate = False)
 
 def tan(x):
     x = Measure(x)
@@ -66,7 +66,7 @@ def tan(x):
 
     value = np.tan(x._value)
     error = (1+value**2) * x._error
-    return Measure(value, error, aproximate=False)
+    return Measure(value, error, approximate=False)
 
 
 def asin(x):
@@ -78,7 +78,7 @@ def asin(x):
     value = np.arcsin(x._value)
     error = x._error/np.sqrt(1-np.power(x._value, 2)) if (x._value != 1).any() else np.abs(np.arcsin(x._value-x._error) - value)
 
-    return Measure(value, error, aproximate=False, units=units.rad)
+    return Measure(value, error, approximate=False, units=units.rad)
 
 
 def acos(x):
@@ -90,7 +90,7 @@ def acos(x):
     value = np.arccos(x._value)
     error = x._error/np.sqrt(1-np.power(x._value, 2)) if (x._value != 1).any() else np.abs(np.arcsin(x._value-x._error) - value)
 
-    return Measure(value, error, aproximate=False, units=units.rad)
+    return Measure(value, error, approximate=False, units=units.rad)
 
 
 def atan(x):
@@ -101,7 +101,7 @@ def atan(x):
 
     value = np.arctan(x._value)
     error = x.error/(1+np.power(x._value, 2))
-    return Measure(value, error, aproximate=False, units=units.rad)
+    return Measure(value, error, approximate=False, units=units.rad)
 
 def atan2(x: Measure, y: Measure):
     """
@@ -121,7 +121,7 @@ def atan2(x: Measure, y: Measure):
 
     error = np.sqrt((y._value*x._error)**2+(x._value*y._error)**2)/np.abs(x**2+y**2)
 
-    return Measure(angles, error, aproximate=False, units=units.rad)
+    return Measure(angles, error, approximate=False, units=units.rad)
 
 def ln(x: Measure) -> Measure:
     """
@@ -136,7 +136,7 @@ def ln(x: Measure) -> Measure:
 
     value = np.log(x._value)
     error = abs(1/x._value)*x._error
-    return Measure(value, error, aproximate = False)
+    return Measure(value, error, approximate = False)
 
 def sqrt(x: Measure) -> Measure:
     """
@@ -157,7 +157,7 @@ def exp(x: Measure) -> Measure:
 
     value = np.exp(x._value)
     error = abs(value)*x._error
-    return Measure(value, error, aproximate=False)
+    return Measure(value, error, approximate=False)
 
 def delta(x: Measure) -> Measure:
     """
@@ -171,7 +171,7 @@ def delta(x: Measure) -> Measure:
         v = i-j
         values.append(v._value[0])
         errors.append(v._error[0])
-    return Measure(values, errors, units = x.units, aproximate=False)
+    return Measure(values, errors, units = x.units, approximate=False)
 
 if __name__ == '__main__':
     print(cos(acos(Measure(1, 0.1))))

pysics/objects.py
@@ -1,8 +1,8 @@
-from .aprox import aprox
+from .approx import approx
 from .statistic import mean, standard_deviation, standard_error
 import numpy as np
 from math import nan
-from . import calculos
+from . import round
 from typing import Self
 
 type number = int | float
@@ -122,8 +122,8 @@ class Unit:
         return self**-1*other
 
     def __str__(self):
-        if self.scale != 1:
-           return str(self.scale) + "  " + self.symbol
+        if self.value != 1:
+            return str(self.value) + " " + self.symbol
         return self.symbol
 
     def __repr__(self):
@@ -151,7 +151,7 @@ class Measure:
     Basic object to store values. It can be given one or several values
     (in a list) and their respective errors
     """
-    def __init__(self, value: listable , error: listable | None = None, aproximate: bool = True, units = None):
+    def __init__(self, value: listable , error: listable | None = None, approximate: bool = True, units = None):
         if isinstance(value, Unit):
             units = Unit(value.si, scale=value.scale, value=1, symbol=value.symbol)
             value = np.array([value.value])
@@ -181,18 +181,18 @@ class Measure:
 
         self.units = units
 
-        if aproximate:
-            self.aprox()
+        if approximate:
+            self.approx()
         self.__print_style = self.Style.pm
 
     @classmethod
-    def from_pairs(cls, *args, aproximate=False) -> Self:
+    def from_pairs(cls, *args, approximate=False) -> Self:
         """
         Given a group of pairs of values with their errors, it returns the corresponding value
         """
         if not all([len(i) == 2 for i in args]):
             raise TypeError(f"Expected pairs of numbers but at least one of them isnt")
-        return cls([i[0] for i in args], [i[1] for i in args], aproximate=aproximate)
+        return cls([i[0] for i in args], [i[1] for i in args], approximate=approximate)
 
     @property
     def value(self) -> list[float]:
@@ -210,21 +210,21 @@ class Measure:
 
     def list_of_measures(self) -> list["Measure"]:
         """Returns a list with the values contained as individual values"""
-        return [Measure(*i, aproximate=False, units=self.units).change_style(self.__print_style) for i in zip(self._value, self._error)]
+        return [Measure(*i, approximate=False, units=self.units).change_style(self.__print_style) for i in zip(self._value, self._error)]
 
     def copy(self) -> "Measure":
         """Returns an independent copy of itself. All the pointers to the data are different"""
         # the list are to make the lists independent
-        return Measure(list(self._value), list(self._error), aproximate=False, units=self.units).change_style(self.__print_style)
+        return Measure(list(self._value), list(self._error), approximate=False, units=self.units).change_style(self.__print_style)
 
-    def aprox(self, decimals = None) -> Self:
-        """Aproximate the values of the value"""
+    def approx(self, decimals = None) -> Self:
+        """approximate the values of the value"""
         # The list are to make the lists independent
         if decimals is None:
-            self._value, self._error = aprox(self._value, self._error)
+            self._value, self._error = approx(self._value, self._error)
         else:
-            self._value = np.array([calculos.round(i, decimals) for i in self._value])
-            self._error = np.array([calculos.round(i, decimals) for i in self._error])
+            self._value = np.array([round.round(i, decimals) for i in self._value])
+            self._error = np.array([round.round(i, decimals) for i in self._error])
 
         return self
 
@@ -257,7 +257,7 @@ class Measure:
         """Calculates the mean of the values of the value and estimates the error by comparing
         the standard error and the mean error and takes the larger of the two"""
         mean_error = np.sqrt(np.sum(self._error**2))/len(self._error)
-        return Measure(self.mean(), list(np.max([self.standard_error(), mean_error])), aproximate = False)
+        return Measure(self.mean(), list(np.max([self.standard_error(), mean_error])), approximate = False)
 
     def change_style(self, style) -> Self:
         """Changes the current style for another"""
@@ -324,7 +324,7 @@ class Measure:
 
 # -----------------------------------------------------------------------------
     def __abs__(self):
-        return Measure(abs(self._value), self._error, aproximate=False, units=self.units)
+        return Measure(abs(self._value), self._error, approximate=False, units=self.units)
 
 
     def __add__(self, other):
@@ -338,7 +338,7 @@ class Measure:
         self = self.convert(finalunits)
         other = other.convert(finalunits)
 
-        return Measure(self._value + other._value, np.sqrt(self._error**2 + other._error**2), aproximate = False, units=finalunits)
+        return Measure(self._value + other._value, np.sqrt(self._error**2 + other._error**2), approximate = False, units=finalunits)
 
     def __radd__(self, other):
         return self + other
@@ -354,7 +354,7 @@ class Measure:
         self = self.convert(finalunits)
         other = other.convert(finalunits)
 
-        return Measure(self._value - other._value, np.sqrt(self._error**2 + other._error**2), aproximate = False, units = finalunits)
+        return Measure(self._value - other._value, np.sqrt(self._error**2 + other._error**2), approximate = False, units = finalunits)
 
     def __rsub__(self, other):
         return -self + other
@@ -366,10 +366,10 @@ class Measure:
         error = np.sqrt(np.array( (other._value * self._error)**2 + (self._value * other._error)**2 ))
         units = self.units*other.units
 
-        return Measure(value, error, aproximate = False, units=units)
+        return Measure(value, error, approximate = False, units=units)
 
     def __rmul__(self, val):
-        return Measure(val * self._value, abs(val) * self._error, aproximate = False)
+        return Measure(val * self._value, abs(val) * self._error, approximate = False)
 
     def __truediv__(self, other):
         return self*other**-1
@@ -381,10 +381,10 @@ class Measure:
         value = self._value**other
         error = abs((other)*self._value**(other-1))*self._error
         units = self.units**other
-        return Measure(value, error, aproximate = False, units=units)
+        return Measure(value, error, approximate = False, units=units)
 
     def __and__(self, other):
-        return Measure(self._value + other._value, self._error + other._error, aproximate = False, units=self.units)
+        return Measure(self._value + other._value, self._error + other._error, approximate = False, units=self.units)
 
     def __or__(self, other):
         return self & -other
@@ -399,7 +399,7 @@ class Measure:
 
     def __getitem__(self, index):
         if not hasattr(index, '__getitem__'):
-            return Measure(self._value[index], self._error[index], aproximate=False)
+            return Measure(self._value[index], self._error[index], approximate=False)
 
         expected_index = index[0]
         value_o_error = index[1]
@@ -466,16 +466,16 @@ class Line:
     ordinate in the origin. It can be deconstructed as a tuple (slope, n_0)
     """
     def __init__(self, slope=0, n_0=0, x=[]):
-        self.slope = Measure(slope, aproximate = False)
-        self.n_0 = Measure(n_0, aproximate = False)
+        self.slope = Measure(slope, approximate = False)
+        self.n_0 = Measure(n_0, approximate = False)
         if not isinstance(x, Measure):
             self.x = Measure(x)
         else:
             self.x = x
 
-    def aprox(self):
-        self.slope.aprox()
-        self.n_0.aprox()
+    def approx(self):
+        self.slope.approx()
+        self.n_0.approx()
         return self
 
     def copy(self):
@@ -512,5 +512,5 @@ if __name__ == '__main__':
     print()
     m = Measure([353.72, 1532.6, 632], [2.56, 1, 1])
     print(f'm -> {m}')
-    m.cambia_estilo(Measure.Estilo.lista)
+    m.change_style(Measure.Style.list)
     print(m)